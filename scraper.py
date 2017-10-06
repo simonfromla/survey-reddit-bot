2cf789fdced520008ee901b6bf33918959a942d0
sjung <sjung2393@gmail.com>
2017-10-06 23:51:54 +0700

    Need factor out functions, clean-up

--- scraper.py
+++ scraper.py
@@ -3,14 +3,7 @@ import praw
 
 
 """
-2) open the json and read each url shortlink.
-request the shortlink page
-if response, scrape comment text, comment id, author
-write to csv according to the url shortlink/subreddit
-CHECKING for updates.
 if new comment, add it. if same comment id but edited answer, add edit to csv.
-
-[description]
 """
 
 def login():
@@ -30,46 +23,23 @@ def scrape(reddit):
     with open("data.json", "r") as f:
         data = json.load(f)
 
-    # shortlinks = []
-    shortlinks = [data[link][0]["shortlink"][-6:] for link in data]
-    # print(shortlinks)
+        shortlinks = [data[link][0]["shortlink"][-6:] for link in data]
 
-    # submission = reddit.submission(url=data["postpreview"][0]["shortlink"])
-    for sl_id in shortlinks:
-        submission = reddit.submission(id=sl_id)
-        submission.comments.replace_more(limit=0)
+        for sl_id in shortlinks:
+            submission = reddit.submission(id=sl_id)
+            submission.comments.replace_more(limit=0)
 
-        cmt_data = [{comment.id: comment.body} for comment in submission.comments.list()]
-        # print(cmtd)
-        # for r in data:
-        #     if sl_id in data[r][0]["shortlink"]:
-        #         data[r][0]["responses"] = cmtd
-        for i in data:
-            if sl_id in data[i][0]["shortlink"]:
-                data[i][1]["responses"] = cmt_data
+            cmt_data = [{comment.id: comment.body} for comment in submission.comments.list()]
 
-        # for key, value in data.items():
-        #     for x in value:
-        #         if sl_id in x['shortlink']:
-        #             if not "responses":
-        #                 x["responses"]= cmt_data
-        #             else:
-        #         # in case you overwrite the response with empty data
-        #                 x['responses'] += cmt_data
+            for i in data:
+                if sl_id in data[i][0]["shortlink"]:
+                    data[i][1]["responses"] = cmt_data
 
 
-    # print(data[r][0]["responses"] for r in data)
+    with open("data.json", "w") as fp:
+        json.dump(data, fp)
+
     print(data)
-    # print(data)
-    # for comment in submission.comments.list():
-    #     print(comment.id, comment.body)
-        # data["postpreview"][1]["responses"][comment.id] = comment.body
-
-    # for comment in submission.comments.list():
-    #     for i, d in data["postpreview"][1]["responses"][i]:
-    #         d[comment.id] = comment.body
-    # print(data)
-
 
 
 # def scrape_response():
