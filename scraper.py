12a3e8983117b7f25135178bb57f8029331a950b
sjung <sjung2393@gmail.com>
2017-10-06 16:51:11 +0700

    Dict outputs additional data

--- scraper.py
+++ scraper.py
@@ -12,19 +12,50 @@ if new comment, add it. if same comment id but edited answer, add edit to csv.
 
 [description]
 """
+
+def login():
+    print("Authenticating")
+    reddit = praw.Reddit("surveyor", user_agent="Chrome:com.example.surveyor-bot:v1 (by /u/man-scout)")
+    print("Authenticated as {}".format(reddit.user.me()))
+    return reddit
+
+
 def deserialize():
     with open("data.json", "r") as f:
         data = json.load(f)
         print(data["postpreview"][0]["shortlink"])
 
 
-def scrape():
+def scrape(reddit):
     with open("data.json", "r") as f:
         data = json.load(f)
-        # print(data["postpreview"][0]["shortlink"])
 
-        submission = reddit.submission(url=data["postpreview"][0]["shortlink"])
-        print(submission)
+    # shortlinks = []
+    shortlinks = [data[link][0]["shortlink"][-6:] for link in data]
+    # print(shortlinks)
+
+    # submission = reddit.submission(url=data["postpreview"][0]["shortlink"])
+    for sl_id in shortlinks:
+        submission = reddit.submission(id=sl_id)
+        submission.comments.replace_more(limit=0)
+
+        cmtd = [{comment.id: comment.body} for comment in submission.comments.list()]
+        # print(cmtd)
+        for r in data:
+            if sl_id in data[r][0]["shortlink"]:
+                data[r][0]["responses"] = cmtd
+    # print(data[r][0]["responses"] for r in data)
+    print(data)
+    # print(data)
+    # for comment in submission.comments.list():
+    #     print(comment.id, comment.body)
+        # data["postpreview"][1]["responses"][comment.id] = comment.body
+
+    # for comment in submission.comments.list():
+    #     for i, d in data["postpreview"][1]["responses"][i]:
+    #         d[comment.id] = comment.body
+    # print(data)
+
 
 
 # def scrape_response():
@@ -40,8 +71,9 @@ def scrape():
 
 
 def main():
-    scrape()
+    reddit = login()
 
+    scrape(reddit)
 
 if __name__ == "__main__":
     main()
